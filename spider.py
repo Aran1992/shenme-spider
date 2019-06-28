a3cf7f81815e8d70046beee45f1bb972bd3acf6c
Aran1992 <2572261667@qq.com>
2019-06-28 14:23:37 +0800

    修复BUG

--- spider.py
+++ spider.py
@@ -5,18 +5,25 @@ import datetime
 import time
 import traceback
 
+
+# todo 将导入的网址关键词进行合并 同一个关键词只查询一次 然后在其中搜索对应的域名
+
+def get_cur_time_filename():
+    return time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+
+
 PAGE = 10
 TIMEOUT = 1
 
-global_url = ""
-global_text = ""
-global_output = []
+global_url = ''
+global_text = ''
+global_file_name = '关键词排名-%s.xlsx' % get_cur_time_filename()
 
 
 def main():
-    for (domain, keyword) in get_input():
-        get_rank(domain, keyword)
-    return set_output()
+    init_output()
+    for i, (domain, keyword) in enumerate(get_input()):
+        get_rank(i + 1, domain, keyword)
 
 
 def get_input():
@@ -30,14 +37,14 @@ def get_input():
     return result
 
 
-def get_rank(domain, keyword):
-    print('开始抓取 域名：%s 关键词：%s' % (domain, keyword))
+def get_rank(index, domain, keyword):
+    print('开始抓取 第%s条 域名：%s 关键词：%s' % (index, domain, keyword))
     for i in range(PAGE):
         get_page(domain, keyword, i + 1)
 
 
 def get_page(domain, keyword, page):
-    global global_url, global_text, global_output
+    global global_url, global_text
     print('开始第%d页' % page)
     # todo 这个网址这样请求就一定能够返回想要的结果吗？
     params = {
@@ -53,6 +60,7 @@ def get_page(domain, keyword, page):
     # todo 要怎么模拟真实的用户进行搜索 HEAD要怎么填写
     headers = {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+        # 'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36'
     }
     # todo 网络出现问题的时候怎么办？
     # todo 抓取的内容有问题的时候怎么办？
@@ -61,15 +69,16 @@ def get_page(domain, keyword, page):
     while r is None or soup is None:
         try:
             r = requests.get('http://so.m.sm.cn/s', params=params, headers=headers)
-        except requests.exceptions.ConnectionError:
+        except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError):
             print('检查到网络断开，%s秒之后尝试重新抓取' % TIMEOUT)
             time.sleep(TIMEOUT)
+            continue
+        global_url = r.url
+        global_text = r.text
         soup = BeautifulSoup(r.text, 'lxml')
         if soup.body is None:
             print('请求到的页面的内容为空，将再次进行请求')
             soup = None
-    global_url = r.url
-    global_text = r.text
     all_item = get_all_item(soup)
     result = []
     rank = 1
@@ -84,7 +93,7 @@ def get_page(domain, keyword, page):
                 datetime.datetime.now()
             ))
         rank += 1
-    global_output += result
+    set_output(result)
 
 
 def get_all_item(soup):
@@ -116,21 +125,21 @@ def get_title(item):
             return span.text
 
 
-def set_output():
-    global global_output
+def init_output():
+    global global_file_name
     wb = Workbook()
     ws = wb.active
     ws.append(('域名', '关键词', '搜索引擎', '排名', '真实地址', '标题', '查询时间'))
-    for (domain, keyword, rank, url, title, date_time) in global_output:
+    wb.save(global_file_name)
+
+
+def set_output(result):
+    global global_file_name
+    wb = load_workbook(filename=global_file_name)
+    ws = wb.active
+    for (domain, keyword, rank, url, title, date_time) in result:
         ws.append((domain, keyword, '神马', rank, url, title, date_time))
-    file_name = '关键词排名-%s.xlsx' % get_cur_time_filename()
-    # todo 查询一定数量之后就保存下来
-    wb.save(file_name)
-    return file_name
-
-
-def get_cur_time_filename():
-    return time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+    wb.save(global_file_name)
 
 
 def error_exit(title):
@@ -145,13 +154,13 @@ def error_exit(title):
 
 
 try:
-    file_name = main()
-    input('查询结束，查询结果保存在 %s' % file_name)
+    main()
+    input('查询结束，查询结果保存在 %s' % global_file_name)
 except:
     filename = 'error-%s.log' % get_cur_time_filename()
-    f = open(filename, 'w', encoding="utf-8")
+    f = open(filename, 'w', encoding='utf-8')
     f.write('''%s
-    
+
 请求的URL为：
 %s
 
@@ -160,5 +169,4 @@ except:
 ''' % (traceback.format_exc(), global_url, global_text))
     f.close()
     traceback.print_exc()
-    set_output()
     input('请将最新的error.log文件发给技术人员')
